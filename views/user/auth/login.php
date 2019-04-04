763f1f4f5586c23437744181a61f1fc1f277f054
root <root@corex.local>
2019-04-04 16:59:12 +0000

    mobirise landing page

--- views/user/auth/login.php
+++ views/user/auth/login.php
@@ -1,150 +1,157 @@
 <?php
-use \yii\helpers\Url;
-use yii\widgets\ActiveForm;
-use \humhub\compat\CHtml;
 
-$this->pageTitle = Yii::t('UserModule.views_auth_login', 'Login');
-$this->registerCssFile('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
-$this->registerCssFile('https://fonts.googleapis.com/css?family=Open+Sans');
-$this->registerJsFile('https://code.jquery.com/jquery-3.3.1.min.js');
-$this->registerJsFile('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js');
-?>
+  $this->registerCssFile('/themes/Freeflow/assets/web/assets/mobirise-icons/mobirise-icons.css');
+  $this->registerCssFile('/themes/Freeflow/assets/tether/tether.min.css');
+  $this->registerCssFile('/themes/Freeflow/assets/bootstrap/css/bootstrap.min.css');
+  $this->registerCssFile('/themes/Freeflow/assets/bootstrap/css/bootstrap-grid.min.css');
+  $this->registerCssFile('/themes/Freeflow/assets/bootstrap/css/bootstrap-reboot.min.css');
+  $this->registerCssFile('/themes/Freeflow/assets/dropdown/css/style.css');
+  $this->registerCssFile('/themes/Freeflow/assets/animatecss/animate.min.css');
+  $this->registerCssFile('/themes/Freeflow/assets/socicon/css/styles.css');
+  $this->registerCssFile('/themes/Freeflow/assets/theme/css/style.css');
+  $this->registerCssFile('/themes/Freeflow/assets/mobirise/css/mbr-additional.css');
+  use yii\helpers\Url;
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="d-none d-sm-block col-md-5">
-            <div class="instructure">
-                <img src="<?php echo Url::base(); ?>/themes/Freeflow/img/Freeflow-white-01.png">
+?>  
+  
+  
+  <section class="menu cid-rm82XpYxpi" once="menu" id="menu2-7">
 
-                <div class="inst-text text-white">
-                    <ul class="list-unstyled ml-5" id="regi-text">
-                        <li>Step 1: Register with <span class="iyo p-0 text-uppercase font-weight-bold">ItsYou.<span class="font-weight-light">Online</span></span>
-                            <span class="text-uppercase font-weight-bold">safe . secure . private</span>
-                        </li>
-                        <li>Step 2: Fill in your data</li>
-                        <li>Step 3: Confirm your identity</li>
-                        <li>Step 4: Welcome to FreeFlowPages!</li>
-                    </ul>
+    
 
-                    <ul class="list-unstyled ml-5" id="sign">
-                        <li>Login with <span class="iyo p-0 text-uppercase font-weight-bold">ItsYou.<span class="font-weight-light">Online</span></span> OR</li>
-                        <li>Login with Username/Password</li>
-                    </ul>
-                </div>
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://www2.freeflowpages.com/">
+                        <img src="/themes/Freeflow/assets/images/freeflow-color-2.png" alt="freeflowpages">
+                    </a>
+                </span>
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+        </div>
+ 
+    </nav>
+</section>
+
+<section class="header11 cid-rkGoZZVpNp mbr-fullscreen" id="header11-0">
+
+    <!-- Block parameters controls (Blue "Gear" panel) -->
+    
+    <!-- End block parameters -->
+    <div class="container align-left">
+        <div class="media-container-column mbr-white col-md-12 py-5">
+            <h3 class="mbr-section-subtitle py-3 mbr-fonts-style display-5"><strong><span style="font-style: normal;">DECENTRALIZED </span></strong><strong><span style="font-style: normal;">SOCIAL SPACES</span>&nbsp;</strong></h3>
+            <h1 class="mbr-section-title py-3 mbr-fonts-style display-1"><strong>FreeFlowPages&nbsp</strong>where privacy and social media co-exist.</h1>
+
+		   
+	    <div class="mbr-section-btn py-3"><a class="btn btn-md btn-white-outline display-4" href="https://itsyou.online/register?client_id=tf_freeflowpages&endpoint=/v1/oauth/authorize&redirect_uri=https://freeflowpages.com/user/auth/external?authclient=itsyouonline&response_type=code&scope=user:name,user:email&xoauth_displayname=freeflowpages#/' ?>">SIGNUP</a> <a class="btn btn-md btn-white-outline display-4" href="/user/auth/external?authclient=itsyouonline">LOGIN</a></div>
+              <span class="ml-3">Secured by <u><a href="https://itsyou.online/" class="iyo p-0 text-up.percase font-weight-bold text-white" target="_blank">itsyou.<span class="font-weight-light">online</span> </a></u><span>a secure and private identity service</span></span>
+        </div>
+    </div>
+
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="mbr-section form3 cid-rl4HgSgAj9" id="form3-2">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-lg-8">
+                <h2 class="align-center pb-2 mbr-fonts-style">
+                    <strong>Invite a friend</strong>
+                </h2>
+                
             </div>
         </div>
 
-        <div class="col-md-7">
-            <div class="sign-card" id="login-form">
-                <img src="<?php echo Url::base(); ?>/themes/Freeflow/img/Freeflow-white-01.png">
-
-                <div class="inst-text text-white">
-                    <ul class="list-unstyled ml-5" id="regi-text-sm">
-                        <li>Step 1: Register with <span class="iyo p-0 text-uppercase font-weight-bold">ItsYou.<span class="font-weight-light">Online</span></span>
-                            <span class="text-uppercase font-weight-bold">safe . secure . private</span>
-                        </li>
-                        <li>Step 2: Fill in your data</li>
-                        <li>Step 3: Confirm your identity</li>
-                        <li>Step 4: Welcome to FreeFlowPages!</li>
-                    </ul>
-
-                    <ul class="list-unstyled ml-5" id="sign-sm">
-                        <li>Login with <span class="iyo p-0 text-uppercase font-weight-bold">ItsYou.<span class="font-weight-light">Online</span></span> OR</li>
-                        <li>Login with Username/Password</li>
-                    </ul>
-                </div>
-
-                <div class="form-box animated bounceIn">
-                    <ul class="nav nav-tabs" id="myTab" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" id="signin-tab" data-toggle="tab" href="#signin" role="tab" aria-controls="signin" aria-selected="true">Sign In</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="register-tab" data-toggle="tab" href="#register" role="tab" aria-controls="register" aria-selected="false">Sign Up</a>
-                        </li>
-                    </ul>
-
-                    <div class="tab-content" id="myTabContent">
-                        <div class="tab-pane fade show active text-center my-5" id="signin" role="tabpanel" aria-labelledby="signin">
-                            <a href="<?php echo Url::base(); ?>/user/auth/external?authclient=itsyouonline" data-pjax-prevent class="btn text-white">Sign in
-                                with <span class="iyo p-0 text-uppercase font-weight-bold bg-white">ItsYou.<span class="font-weight-light">Online</span></span></a>
-
-                            <hr>
-                            <span>OR</span>
-
-
-                            <?php if (Yii::$app->session->hasFlash('error')) : ?>
-                            <div class="alert alert-danger" role="alert">
-                                <?= Yii::$app->session->getFlash('error') ?>
-                            </div>
-                            <?php endif; ?>
-
-                            <?php $form = ActiveForm::begin(['id' => 'account-login-form', 'options' => ['class' => "mt-3"], 'enableClientValidation' => false]); ?>
-                            <div class="form-group">
-                                <?= $form->field($model, 'username')->textInput(['id' => 'login_username', 'placeholder' => $model->getAttributeLabel('username'), 'options' => ["class" => "form-control"], 'aria-label' => $model->getAttributeLabel('username')])->label(false); ?>
-                                <?= $form->field($model, 'password')->passwordInput(['id' => 'login_password', 'placeholder' => $model->getAttributeLabel('password'), 'options' => ["class" => "form-control"], 'aria-label' => $model->getAttributeLabel('password')])->label(false); ?>
-                            </div>
-                            <div class="custom-control custom-checkbox float-left">
-                                <?= $form->field($model, 'rememberMe')->checkbox(); ?>
-                            </div>
-                            <?= CHtml::submitButton(Yii::t('UserModule.views_auth_login', 'Sign in'), ['id' => 'login-button', 'class' => 'btn btn-primary btn-md btn-block']); ?>
-
-                            <a id="password-recovery-link" class="password my-4" href="<?= Url::toRoute('/user/password-recovery'); ?>" data-pjax-prevent><br><?= Yii::t('UserModule.views_auth_login', 'Forgot your password?') ?></a>
-
-                            <?= humhub\widgets\LanguageChooser::widget(); ?>
-
-                            <?php ActiveForm::end(); ?>
-                        </div>
-
-                        <div class="tab-pane fade text-center my-5" id="register" role="tabpanel" aria-labelledby="register-tab">
-                            <a href="<?php echo Url::base(); ?>/user/auth/external?authclient=itsyouonline" class="btn btn-secondary text-white">Register
-                                with <span class="iyo p-0 text-uppercase font-weight-bold">ItsYou.<span class="font-weight-light">Online</span></span></a>
+        <div class="row py-2 justify-content-center">
+            <div class="col-12 col-lg-6  col-md-8 ">
+                <!---Formbuilder Form--->
+                <form method="POST" class="mbr-form form-with-styler">                    <div class="row">
+                        <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Invitation sent.</div>
+                        <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
                         </div>
                     </div>
-                </div>
+                    <div class="dragArea row">
+                        <div class="form-group col" data-for="email">
+			    <input type="email" name="invite[email]" placeholder="Email" data-form-field="Email" required="required" class="form-control display-7" id="email-form3-2">
+			     <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />			
+</div>
+                        <div class="col-auto input-group-btn"><button type="submit" class="btn  btn-primary display-4">INVITE</button></div>
+                    </div>
+                </form><!---Formbuilder Form--->
             </div>
         </div>
     </div>
-</div>
+</section>
 
-<script type="text/javascript">
-    $(function() {
-        // set cursor to login field
-        $('#login_username').focus();
-    });
-    // Shake panel after wrong validation
-    <?php if ($model->hasErrors()) { ?>
-    $('#login-form').removeClass('bounceIn');
-    $('#login-form').addClass('shake');
-    $('#app-title').removeClass('fadeIn');
-    <?php
-} ?>
-    // Shake panel after wrong validation
-    <?php if ($invite->hasErrors()) { ?>
-    $('#login-form').removeClass('bounceIn');
-    $('#app-title').removeClass('fadeIn');
-    <?php
-} ?>
 
-    let nav_tabs = document.querySelector('.nav-tabs');
-    nav_link = document.querySelector('.nav-link');
-    register = document.getElementById('regi-text');
-    signin = document.getElementById('sign');
-    registersm = document.getElementById('regi-text-sm');
-    signinsm = document.getElementById('sign-sm');
+<section once="footers" class="cid-rm88OKgSeI" id="footer7-8">
 
-    nav_tabs.addEventListener('click', function(e) {
-        if (e.target.id == 'signin-tab') {
-            register.style.display = 'none';
-	    signin.style.display = 'block';
-	    registersm.style.display = 'none';
-            signinsm.style.display = 'block';
-        } else if (e.target.id == 'register-tab') {
-            signin.style.display = 'none';
-	    register.style.display = 'block';
-	    signinsm.style.display = 'none';
-            registersm.style.display = 'block';
-        }
-    });
-</script>
+    
 
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7"><a class="mbr-bold text-primary" href="/s/freeflowpages/" target="_blank">FreeFlowPages</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a class="mbr-bold text-primary" href="https://github.com/freeflowpages" target="_blank">GitHub</a></li></ul>
+            </div>
+            
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                    © Copyright 2019 FreeFlowPages - All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section once="" class="cid-rm82Mm2JT0" id="footer6-6">
+
+
+    <div class="mbr-overlay" style="opacity: 0.2; background-color: rgb(35, 35, 35);"></div>
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7"><strong>Hosted On The <a href="http://www.threefold.io" target="_blank" class="text-white" style="font-style: italic;"><u></u></a><u><a href="http://www.threefold.io" target="_blank" class="text-white"><b>ThreeFold</b> <b>Grid</b></a></u></strong></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+  <script src="/themes/Freeflow/assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="/themes/Freeflow/assets/popper/popper.min.js"></script>
+  <script src="/themes/Freeflow/assets/tether/tether.min.js"></script>
+  <script src="/themes/Freeflow/assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="/themes/Freeflow/assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="/themes/Freeflow/assets/dropdown/js/script.min.js"></script>
+  <script src="/themes/Freeflow/assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="/themes/Freeflow/assets/viewportchecker/jquery.viewportchecker.js"></script>
+  <script src="/themes/Freeflow/assets/theme/js/script.js"></script>
+  <script src="/themes/Freeflow/assets/formoid/formoid.min.js"></script>
+  
+  
+  <input name="animation" type="hidden">
