8a12da1c5a9250e1aeddeac501e68ee0c8c295d3
hamdy <hamdy.a.farag@gmail.com>
2019-05-21 15:36:59 +0200

    Upgrade insecure requests

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,7 +9,8 @@
 <html lang="<?= Yii::$app->language ?>">
     <head>
         <title><?= strip_tags($this->pageTitle); ?></title>
-        <meta charset="<?= Yii::$app->charset ?>">
+	 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+	<meta charset="<?= Yii::$app->charset ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <?php $this->head() ?>
         <?= $this->render('head'); ?>
