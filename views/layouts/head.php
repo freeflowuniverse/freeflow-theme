0a88c55fb7c991aa918ca7c708465ef9fbf4c978
samaradel <samar@incubaid.com>
2019-12-04 14:03:30 +0200

    Add Crisp

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -31,3 +31,5 @@
 <!-- start: Android Fullscreen -->
 <meta name="mobile-web-app-capable" content="yes">
 <!-- end: Android Fullscreen -->
+
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="1a5a5241-91cb-4a41-8323-5ba5ec574da0";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
